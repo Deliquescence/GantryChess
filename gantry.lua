c2846c51373c24adf82d15f06d20db77d46a6f24
Josh Baird <jjb.pub01@protonmail.com>
2023-12-27 14:21:23 -0500

    Initialization

--- gantry.lua
+++ gantry.lua
@@ -1,9 +1,7 @@
-
-
- -- Primary: forward/back
+-- Primary: forward/back
 SIDE_PRIMARY_AXIS = "left"
 -- Secondary: left/right
-SIDE_SECONDARY_AXIS = "back" 
+SIDE_SECONDARY_AXIS = "back"
 SIDE_STICKER = "right"
 SIDE_GEARSHIFT = "top"
 -- clockwise input into gearshift
@@ -20,8 +18,30 @@ function init()
         redstone.setOutput(side, false)
     end
 
+    function init_axis(axis)
+        while true do
+            local _id, message = rednet.receive(PROTOCOL, 5)
+            if message == nil then
+                print("No message within timeout, sending broadcast")
+                rednet.broadcast("UPDATE_LOCATION", PROTOCOL)
+            elseif message == axis.."_0" then
+                print(axis.." axis initialization successful")
+                break
+            end
+        end
+    end
+
+
+    print("Attempting primary axis initialization")
     move_backward()
+    init_axis("PRIMARY")
+
+    print("Attempting secondary axis initialization")
+    move_left()
+    init_axis("SECONDARY")
 end
+
+
 
 function move_forward()
     redstone.setOutput(SIDE_PRIMARY_AXIS, false)
@@ -61,4 +81,4 @@ return {
     move_left = move_left,
     move_right = move_right,
     halt_movement = halt_movement,
- }
+}
