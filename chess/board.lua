bdfc99a1c32c95f29062e3f33eaa20bc3437d236
Josh Baird <jjb.pub01@protonmail.com>
2024-01-07 23:19:13 -0500

    Add functions for file, rank, ray

--- chess/board.lua
+++ chess/board.lua
@@ -22,8 +22,61 @@ function Board:to_string()
     return str
 end
 
+function Board.file_name(file)
+    return string.char(string.byte("a") + file - 1)
+end
+
+function Board.square_name(index)
+    local file = Board.file(index)
+    local rank = Board.rank(index)
+    return Board.file_name(file) .. rank
+end
+
 function Board.index(file, rank)
     return file + ((rank - 1) * 8)
+end
+
+function Board.file(index)
+    return bit.band(index - 1, 7) + 1
+end
+
+function Board.rank(index)
+    return bit.brshift(index - 1, 3) + 1
+end
+
+local function signum(i)
+    if i < 0 then
+        return -1
+    elseif i > 0 then
+        return 1
+    else
+        return 0
+    end
+end
+
+-- from exclusive, to inclusive
+function Board.ray(from, to)
+    local ffile = Board.file(from)
+    local frank = Board.rank(from)
+    local tfile = Board.file(to)
+    local trank = Board.rank(to)
+    local file = ffile
+    local rank = frank
+
+    return function()
+        local horizontal = tfile - file
+        local vertical = trank - rank
+        if math.abs(horizontal) ~= math.abs(vertical) and horizontal ~= 0 and vertical ~= 0 then
+            return
+        end
+        if file == tfile and rank == trank then
+            return
+        end
+
+        file = file + signum(horizontal)
+        rank = rank + signum(vertical)
+        return Board.index(file, rank)
+    end
 end
 
 return Board
--- chess_debug.lua
+++ chess_debug.lua
@@ -1,12 +1,31 @@
 local Fen = require("chess/fen")
 local San = require("chess/san")
+local Board = require("chess/board")
 
 -- local parsed = Fen.parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b Qkq - 0 1")
 local parsed = Fen.parse("5R1Q/8/4p1k1/8/4P1K1/r7/p7/r7 w - - 0 54")
 
-print(textutils.serialize(parsed.board))
-print(parsed.board)
-print(parsed.board:to_string())
+-- print(textutils.serialize(parsed.board))
+-- print(parsed.board)
+-- print(parsed.board:to_string())
+
+-- print(Board.index(1, 1))
+-- print(Board.file(1))
+-- print(Board.rank(1))
+-- print()
+-- print(Board.index(8, 8))
+-- print(Board.file(64))
+-- print(Board.rank(64))
+-- print()
+-- print(Board.index(7, 2))
+-- print(Board.file(15))
+-- print(Board.rank(15))
+
+-- print(Board.square_name(64))
+
+for s in Board.ray(24, 6) do
+    print(Board.square_name(s))
+end
 
 -- print (San.parse_square("a1"))
 -- print (San.parse_square("h8"))
